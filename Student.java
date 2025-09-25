cd400a45afc58d60aca113cfccaff9c58e671123
Arshad Ashrafi <Ashrafiarshad9792@gmail.com>
2025-09-25 14:48:58 -0700

    revert printing values

---
+++ Student.java
@@ -0,0 +1,10 @@
+package GitHub;
+
+public class Student{
+    public static  void main(String []args){
+        int age =24;
+        String name="arshad";
+        System.out.println("age =" +age);
+        System.out.println("name="+name);
+    }
+}
